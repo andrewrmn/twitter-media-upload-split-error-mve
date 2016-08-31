c9b3be86e539fbdd6e5c8d32c7f2d75e2a72f398
Andrew Pinkham <code@andrewsforge.com>
2016-08-31 16:04:02 -0400

    Implement Twitter Chunked Upload

--- main.js
+++ main.js
@@ -3,6 +3,7 @@
 /* eslint no-console: "off" */
 
 var tweet;
+var twitterChunkedUpload;
 var twitterLogin;
 var twitterSimpleUpload;
 
@@ -81,6 +82,80 @@ twitterSimpleUpload = function (base64ImageString, fileSize) {
       });
     }).fail(function (err) {
       console.log('APPEND Failed');
+      console.log(err);
+    });
+  }).fail(function (err) {
+    console.log('INIT Failed');
+    console.log(err);
+  });
+};
+
+twitterChunkedUpload = function (base64ImageString, fileSize, chunkSize) {
+  var mediaIdString;
+  var oAuthRequest = twitterLogin();
+  oAuthRequest.post(
+    'https://upload.twitter.com/1.1/media/upload.json',
+    { data: {
+      command: 'INIT',
+      media_type: 'image/jpeg',
+      total_bytes: fileSize
+    } }
+  ).done(function (twitterResponse) {
+    // INIT successfully sent
+    // upload the one and only chunk to twitter
+    mediaIdString = twitterResponse.media_id_string;
+    oAuthRequest.post(
+      'https://upload.twitter.com/1.1/media/upload.json',
+      {
+        headers: { 'Content-Transfer-Encoding': 'base64' },
+        data: {
+          command: 'APPEND',
+          media_data: base64ImageString.slice(0, chunkSize),
+          media_id: mediaIdString,
+          segment_index: 0
+        }
+      }
+    ).done(function () {
+      // first APPEND successful
+      oAuthRequest.post(
+        'https://upload.twitter.com/1.1/media/upload.json',
+        {
+          headers: { 'Content-Transfer-Encoding': 'base64' },
+          data: {
+            command: 'APPEND',
+            media_data: base64ImageString.slice(
+              chunkSize,
+              base64ImageString.length
+            ),
+            media_id: mediaIdString,
+            segment_index: 1
+          }
+        }
+      ).done(function () {
+        // second APPEND successful
+        oAuthRequest.post(
+          'https://upload.twitter.com/1.1/media/upload.json',
+          { data: {
+            command: 'FINALIZE',
+            media_id: mediaIdString
+          } }
+        ).done(function () {
+          // FINALIZE successful
+          tweet(
+            oAuthRequest,
+            'Chunked (size ' + chunkSize + ') Append!',
+            mediaIdString
+          );
+        }).fail(function (err) {
+          console.log('FINALIZE Failed');
+          console.log(err);
+        });
+      }).fail(function (err) {
+        console.log('second APPEND Failed');
+        console.log(err);
+      });
+    }).fail(function (err) {
+      console.log('first APPEND Failed');
       console.log(err);
     });
   }).fail(function (err) {
