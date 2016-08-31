856eec21c34ad98d22435c595dd38f3aed20dfe6
Andrew Pinkham <code@andrewsforge.com>
2016-08-31 16:01:30 -0400

    Implement tweet function.

--- main.js
+++ main.js
@@ -2,6 +2,7 @@
 /* eslint func-names: "off" */
 /* eslint no-console: "off" */
 
+var tweet;
 var twitterLogin;
 
 twitterLogin = function () {
@@ -19,4 +20,20 @@ twitterLogin = function () {
     return OAuth.create('twitter');
   }
   return oAuthRequest;
+};
+
+tweet = function (oAuthRequest, status, mediaIdString) {
+  oAuthRequest.post(
+    '/1.1/statuses/update.json',
+    { data: {
+      status: status,
+      media_ids: mediaIdString
+    } }
+  ).done(function (postResponse) {
+    console.log('Twitter created tweet with media');
+    console.log(postResponse);
+  }).fail(function (err) {
+    console.log('Twitter could not create tweet');
+    console.log(err);
+  });
 };
